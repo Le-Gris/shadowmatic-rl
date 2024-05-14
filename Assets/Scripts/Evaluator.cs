bf191cbee3d925cc00d502a1b7bf9c2bed822bbd
le-gris <solimlegris@gmail.com>
2024-05-13 22:23:23 -0400

    other stuff

---
+++ Assets/Scripts/Evaluator.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Evaluator : MonoBehaviour
+{
+    GameObject[] agents;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
